bfbf170f46aedc44debec1e3c19c076c83bf2a19
Othman Sefraoui <sefraouiothman7@gmail.com>
2022-01-27 19:49:57 +0100

    merge

--- scripts/take_flashloan.js
+++ scripts/take_flashloan.js
@@ -1,9 +1,8 @@
 async function main() {
     const addr = process.env.FLASHLOAN_CONTRACT_ADDRESS;
-    const flashloan = await hre.ethers.getContractAt("Flashloan", addr);
-
+    const Myflashloan = await hre.ethers.getContractAt("Myflashloan", addr);
     // Take flashloan 
-    await flashloan.flashloan("0xd00ae08403B9bbb9124bB305C09058E32C39A48c", 1)
+    await Myflashloan.flashloan("0xd00ae08403B9bbb9124bB305C09058E32C39A48c")
     console.log("Took a flashloan of 1 WAVAX and payed it back ");
 
 
